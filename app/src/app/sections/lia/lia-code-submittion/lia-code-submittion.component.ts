5051ada17b597e89985b81a1570c5d65019e8437
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-05-26 14:27:58 -0500

    feat(components): Create lia-code-submission component prototype

---
+++ app/src/app/sections/lia/lia-code-submittion/lia-code-submittion.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Lia } from './../lia';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import {MdButton} from '@angular2-material/button';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-lia-code-submittion',
+  templateUrl: 'lia-code-submittion.component.html',
+  styleUrls: ['lia-code-submittion.component.css'],
+  directives: [MD_CARD_DIRECTIVES, MdButton]
+})
+export class LiaCodeSubmittionComponent {
+
+  @Input() lia: Lia;
+  @Output() onLiaSubmitted: EventEmitter<Lia> =  new EventEmitter();
+
+  constructor() {}
+
+  submitLia() {
+    this.onLiaSubmitted.emit(this.lia);
+  }
+
+}
