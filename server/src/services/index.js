d3de304e7631036f054f951fc1b9b66dfa848f9f
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-05-27 16:08:17 -0500

    refactor(server): Refactor LIA ednpoints and support auth validation for LIA submission

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -2,12 +2,13 @@
 const authentication = require('./authentication');
 const user = require('./user');
 const lia = require('./lia');
+const userLia = require('./user/lia');
 
 module.exports = function() {
   const app = this;
   
-  
   app.configure(authentication);
   app.configure(user);
+  app.configure(userLia);
   app.configure(lia);
 };
