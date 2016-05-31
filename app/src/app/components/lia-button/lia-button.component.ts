e3d66f91c5d82e3cedfc5c3c3a255f0771ad9ad5
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-05-31 16:57:45 -0500

    feat(components): Extend lia-button to support lia-button-link directive

--- app/src/app/components/lia-button/lia-button.component.ts
+++ app/src/app/components/lia-button/lia-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewEncapsulation, Type } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -9,3 +9,15 @@ import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/
   encapsulation: ViewEncapsulation.None
 })
 export class LiaButtonComponent {  }
+
+@Component({
+  moduleId: module.id,
+  selector: 'a[lia-button-link]',
+  templateUrl: 'lia-button-link.component.html',
+  styleUrls: ['lia-button.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
+})
+export class LiaButtonLinkComponent {  }
+
+export const LIA_BUTTON_DIRECTIVES: Type[] = [LiaButtonComponent, LiaButtonLinkComponent];
