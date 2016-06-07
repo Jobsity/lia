bb340150fe6d2bbb414db7edd7298909ca80e2d5
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-06-07 17:31:18 -0500

    chore(app): Drop support for angular2-material components and update angular-cli to beta.5

--- app/src/system-config.ts
+++ app/src/system-config.ts
@@ -3,39 +3,11 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
-  '@angular2-material': 'vendor/@angular2-material',
   'moment': 'vendor/moment/moment.js'
 };
 
 /** User packages configuration. */
-const materialPackages:string[] = [
-  'core',
-  'toolbar',
-  'icon',
-  'button',
-  'sidenav',
-  'list',
-  'card',
-  'input',
-  'radio',
-  'checkbox',
-  'grid-list',
-  'progress-bar',
-  'progress-circle'
-];
-
-const packages:any = createCustomConfig(materialPackages);
-
-function createCustomConfig(packages: string[]): any {
-  return packages.reduce((packageConfig: any, packageName: string) => {
-    packageConfig[`@angular2-material/${packageName}`] = {
-      format: 'cjs',
-      defaultExtension: 'js',
-      main: packageName
-    };
-    return packageConfig;
-  }, {});
-}
+const packages: any = { };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
