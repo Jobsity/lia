652de11fc412f459a1692449d2414b2fb60c40ea
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-06-08 14:46:27 -0500

    test(components): Added unit tests for lia-button and lia-button-link components

--- app/src/app/components/lia-button/lia-button.component.spec.ts
+++ app/src/app/components/lia-button/lia-button.component.spec.ts
@@ -9,7 +9,7 @@ import {
 import { ComponentFixture, TestComponentBuilder } from '@angular/compiler/testing';
 import { Component } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { LiaButtonComponent } from './lia-button.component';
+import { LiaButtonComponent, LiaButtonLinkComponent } from './lia-button.component';
 
 describe('Component: LiaButton', () => {
   let builder: TestComponentBuilder;
@@ -24,23 +24,67 @@ describe('Component: LiaButton', () => {
     expect(component).toBeTruthy();
   }));
 
-  it('should create the component', inject([], () => {
-    return builder.createAsync(LiaButtonComponentTestController)
-      .then((fixture: ComponentFixture<any>) => {
+  it('should create the lia-button component', inject([], () => {
+    return builder.createAsync(TestApp).then((fixture: ComponentFixture<any>) => {
         let query = fixture.debugElement.query(By.directive(LiaButtonComponent));
         expect(query).toBeTruthy();
         expect(query.componentInstance).toBeTruthy();
       });
   }));
+
+  it('should contain base css class', inject([], () => {
+    return builder.createAsync(TestApp).then((fixture: ComponentFixture<any>) => {
+      let buttonDebugElement = fixture.debugElement.query(By.css('span'));
+
+      expect(buttonDebugElement.nativeElement.classList.contains('button')).toBeTruthy();
+    });
+  }));
+
+  it('should be able to display button text', inject([], () => {
+    return builder.createAsync(TestApp).then((fixture: ComponentFixture<any>) => {
+      let buttonDebugElement = fixture.debugElement.query(By.css('span'));
+
+      expect(buttonDebugElement.nativeElement.textContent).toBe('Click Me');
+    });
+  }));
+
+  it('should not increment if disabled', inject([], () => {
+    return builder.createAsync(TestApp).then((fixture: ComponentFixture<any>) => {
+      let testComponent = fixture.debugElement.componentInstance;
+      let buttonDebugElement = fixture.debugElement.query(By.css('button'));
+
+      fixture.detectChanges();
+
+      buttonDebugElement.nativeElement.click();
+      expect(testComponent.clickCount).toBe(0);
+    });
+  }));
+
+  describe('a[lia-button-link]', () => {
+    it('should contain desired css styling', inject([], () => {
+      return builder.createAsync(TestApp).then((fixture: ComponentFixture<any>) => {
+        let buttonDebugElement = fixture.debugElement.query(By.css('a span'));
+        expect(buttonDebugElement.nativeElement.classList.contains('dash')).toBeTruthy();
+      });
+    }));
+  });
+
 });
 
 @Component({
-  selector: 'test',
+  selector: 'test-app',
   template: `
-    <app-lia-button></app-lia-button>
+    <button type="button" [disabled]="isDisabled" lia-button (click)="increment()">Click Me</button>
+    <a href="#" lia-button-link>Link button</a>
   `,
-  directives: [LiaButtonComponent]
+  directives: [LiaButtonComponent, LiaButtonLinkComponent]
 })
-class LiaButtonComponentTestController {
+class TestApp {
+  clickCount: number = 0;
+  isDisabled: boolean = true;
+
+  increment() {
+    this.clickCount++;
+  }
 }
 
