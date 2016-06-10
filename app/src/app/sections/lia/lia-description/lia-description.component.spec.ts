14a091e34f53ca0f14f55d1b913a4a736aff2c3f
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-06-10 10:53:31 -0500

    test(components): Added unit tests for lia-description component

--- app/src/app/sections/lia/lia-description/lia-description.component.spec.ts
+++ app/src/app/sections/lia/lia-description/lia-description.component.spec.ts
@@ -25,11 +25,41 @@ describe('Component: LiaDescription', () => {
   }));
 
   it('should create the component', inject([], () => {
-    return builder.createAsync(LiaDescriptionComponentTestController)
+    return builder.createAsync(TestApp)
       .then((fixture: ComponentFixture<any>) => {
         let query = fixture.debugElement.query(By.directive(LiaDescriptionComponent));
         expect(query).toBeTruthy();
         expect(query.componentInstance).toBeTruthy();
+      });
+  }));
+
+  it('should be able to display title', inject([], () => {
+    return builder.createAsync(TestApp)
+      .then((fixture: ComponentFixture<any>) => {
+        fixture.detectChanges();
+        let titleElement = fixture.debugElement.query(By.css('lia-info-title'));
+        
+        expect(titleElement.nativeElement.textContent).toBe('Test title');
+      });
+  }));
+
+  it('should be able to display language', inject([], () => {
+    return builder.createAsync(TestApp)
+      .then((fixture: ComponentFixture<any>) => {
+        fixture.detectChanges();
+        let languageElement = fixture.debugElement.query(By.css('lia-info-language'));
+
+        expect(languageElement.nativeElement.textContent).toBe('javascript');
+      });
+  }));
+
+  it('should be able to display description', inject([], () => {
+    return builder.createAsync(TestApp)
+      .then((fixture: ComponentFixture<any>) => {
+        fixture.detectChanges();
+        let descriptionElement = fixture.debugElement.query(By.css('card-content p'));
+        
+        expect(descriptionElement.nativeElement.textContent).toBe('Test description');
       });
   }));
 });
@@ -37,10 +67,16 @@ describe('Component: LiaDescription', () => {
 @Component({
   selector: 'test',
   template: `
-    <app-lia-description></app-lia-description>
+    <lia-description [lia]="mockLia"></lia-description>
   `,
   directives: [LiaDescriptionComponent]
 })
-class LiaDescriptionComponentTestController {
+class TestApp {
+  mockLia: any = {
+    language: 'javascript',
+    title: 'Test title',
+    time: 10,
+    description: 'Test description'
+  };
 }
 
