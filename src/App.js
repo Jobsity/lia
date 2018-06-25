3fe7aa226b41cd6edb99344f76320511cfa68390
mauricio Lopez <mauriciolopeztam@gmail.com>
2018-06-25 07:36:26 -0500

    New files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="/favicon.ico" className="App-logo" alt="logo" />
+          <h1 className="App-title">LIA</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to LIA
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
