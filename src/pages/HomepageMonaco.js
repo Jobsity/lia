6661d9b1d79f3576c5586e0488fd8d8ad194928f
Pablo Morales <pablo.morales@jobsity.io>
2018-06-29 13:56:56 -0500

    JTAL-228/layout & code editor test

---
+++ src/pages/HomepageMonaco.js
@@ -0,0 +1,139 @@
+import React from "react";
+import RGL, { WidthProvider } from "react-grid-layout";
+import MonacoEditor from 'react-monaco-editor';
+
+const ReactGridLayout = WidthProvider(RGL);
+
+class HomepageMonaco extends React.PureComponent {
+  static defaultProps = {
+    className: "layout",
+    items: 6,
+    rowHeight: 100,
+    onLayoutChange: function() {},
+    cols: 12
+  };
+
+  constructor(props) {
+    super(props);
+
+    const layout = this.generateLayout();
+    this.state = {
+      layout,
+      code: '// type your code...',
+    };
+  }
+
+  editorDidMount(editor, monaco) {
+    console.log('editorDidMount', editor);
+    editor.focus();
+  }
+
+  onChange(newValue, e) {
+   console.log('onChange', newValue, e);
+  }
+
+  generateDOM() {
+    const code = this.state.code;
+    const options = {
+          selectOnLineNumbers: true
+        };
+    return [
+      <div key={"1"}>
+        <MonacoEditor
+          width="100%"
+          language="javascript"
+          theme="vs-dark"
+          value={code}
+          options={options}
+          onChange={this.onChange}
+          editorDidMount={this.editorDidMount}
+        />
+      </div>,
+      <div key={"2"}>
+        <span className="text">{"2"}</span>
+      </div>,
+      <div key={"3"}>
+        <span className="text">{"3"}</span>
+      </div>,
+      <div key={"4"}>
+        <span className="text">{"3"}</span>
+      </div>,
+      <div key={"5"}>
+        <span className="text">{"3"}</span>
+      </div>,
+      <div key={"6"}>
+        <span className="text">{"3"}</span>
+      </div>
+    ];
+  }
+
+  generateLayout() {
+    return [
+      {
+        x: 0,
+        y: 0,
+        w: 8,
+        h: 5,
+        i: "1",
+        static: true
+      },
+      {
+        x: 8,
+        y: 0,
+        w: 4,
+        h: 1,
+        i: "2"
+      },
+      {
+        x: 8,
+        y: 0,
+        w: 4,
+        h: 1.5,
+        i: "3"
+      },
+      {
+        x: 8,
+        y: 0,
+        w: 4,
+        h: 4,
+        i: "4"
+      },
+      {
+        x: 8,
+        y: 0,
+        w: 4,
+        h: 1,
+        i: "5"
+      },
+      {
+        x: 0,
+        y: 1,
+        w: 8,
+        h: 2.5,
+        i: "6"
+      }
+    ];
+  }
+
+  onLayoutChange(layout) {
+    this.props.onLayoutChange(layout);
+  }
+
+  render() {
+    return (
+      <ReactGridLayout
+        layout={this.state.layout}
+        onLayoutChange={this.onLayoutChange}
+        {...this.props}
+      >
+        {this.generateDOM()}
+      </ReactGridLayout>
+    );
+  }
+}
+
+if (require.main === module) {
+  require("../test-hook.jsx")(module.exports);
+}
+
+export default HomepageMonaco;
