3991983f95e5d2ca9cb8f1276ec0e393245f07ba
SebTorres <gsebastorres@outlook.com>
2018-08-02 10:46:40 -0500

    Install socket io. Connect saga

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,17 @@
 import { applyMiddleware, createStore, combineReducers } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
 const rootReducer = combineReducers({
 
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(logger),
+  applyMiddleware(sagaMiddleware, logger),
 );
 
 export default store;
