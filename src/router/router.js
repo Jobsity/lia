e46a5d4844c7da7f110d85f5e9af5c9f7e7e57f4
SebTorres <gsebastorres@outlook.com>
2018-08-02 10:13:57 -0500

    Removed codemirror

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Monaco from '../pages/HomepageMonaco';
-import CodeMirror from '../pages/HomepageCodeMirror';
 
 const Router = () => (
   <Switch>
     <Route exact path="/" component={Monaco} />
-		<Route exact path="/codemirror" component={CodeMirror} />
   </Switch>
 );
 
