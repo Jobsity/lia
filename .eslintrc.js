0f9315160121341a46055a013f1d4d823b0fa3aa
SebTorres <gsebastorres@outlook.com>
2018-08-06 15:38:58 -0500

    Initial test example

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,9 @@ module.exports = {
     },
     "rules": {
       "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    }
+      "import/no-extraneous-dependencies": ["error", {"devDependencies": true, "optionalDependencies": false, "peerDependencies": false}],
+    },
+    "env": {
+      "jest": true
+  }
 };
