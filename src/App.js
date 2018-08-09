e85fe6dff18c508fd78d2d7736173f3d9834e74f
Miguel Isaza <misaji95@hotmail.com>
2018-08-09 16:00:01 -0500

    added welcome screen before main app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import Router from './router/router';
+
+import theme from './styles/theme';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <MuiThemeProvider theme={theme}>
         <Router />
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Homepage from '../pages/Homepage';
+import Welcome from '../components/Welcome/Welcome';
 
 const Router = () => (
   <Switch>
-    <Route exact path="/" component={Homepage} />
+    <Route path="/" exact render={() => <Redirect to="/welcome" />} />
+    <Route exact path="/home" component={Homepage} />
+    <Route exact path="/welcome" component={Welcome} />
   </Switch>
 );
 
