953eeacc2b9cd89776aac1fc349184e6db59e02f
SebTorres <gsebastorres@outlook.com>
2018-08-09 12:27:15 -0500

    Use local script in public folder

--- public/monaco-editor-worker-loader-proxy.js
+++ public/monaco-editor-worker-loader-proxy.js
@@ -2,4 +2,4 @@ self.MonacoEnvironment = {
   baseUrl: '../node_modules/monaco-editor/min',
 };
 
-  importScripts('../node_modules/monaco-editor/min/vs/base/worker/workerMain.js') // eslint-disable-line
+  importScripts('vs/base/worker/workerMain.js') // eslint-disable-line
