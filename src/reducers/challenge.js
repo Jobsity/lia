b8787715f374d75e9d997a1492d1b07d9e382fa2
juliansibaja84 <juliansibaja84@gmail.com>
2018-08-13 16:10:51 -0500

    Added redux store scheme

---
+++ src/reducers/challenge.js
@@ -0,0 +1,11 @@
+const initialState = {
+  difficulty: '',
+  lenguages: [],
+  score: null,
+  stats: null,
+  testsSuite: [],
+};
+
+export default function (state = initialState, action) {
+  return state;
+}
---
+++ src/reducers/session.js
@@ -0,0 +1,13 @@
+const initialState = {
+  isLive: true,
+  language: '',
+  user: {
+    id: '',
+    name: '',
+    role: '',
+  },
+};
+
+export default function (state = initialState, action) {
+  return state;
+}
