6f5ebb3c64b751b3088c45056478155ea3cebefc
Miguel Isaza <misaji95@hotmail.com>
2018-08-16 15:07:26 -0500

    fixed issues

--- src/components/SampleTests/sampleTestsView.js
+++ src/components/SampleTests/sampleTestsView.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import { PropTypes } from 'prop-types';
-import FormControl from '@material-ui/core/FormControl';
+import React from "react";
+import { PropTypes } from "prop-types";
+import FormControl from "@material-ui/core/FormControl";
 
-import Paper from '@material-ui/core/Paper';
-import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import { withStyles } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import { withStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormHelperText from "@material-ui/core/FormHelperText";
 
-import MonacoField from '../MonacoField/MonacoField';
-import styles from './styles';
+import MonacoField from "../MonacoField/MonacoField";
+import styles from "./styles";
 
 function sampleTestsView({
   classes,
@@ -34,29 +33,30 @@ function sampleTestsView({
   handleDialogOpening,
   handleReset,
   handleSubmit,
-  challengeSubmitted,
+  challengeSubmitted
 }) {
   return (
+    <div>
+      {tests.isLoading ? (
         <CircularProgress
           classes={{ root: classes.loading }}
           size={200}
           color="secondary"
         />
       ) : (
-        <Paper square>
+        <div>
           <div className="output-console">
             <MonacoField
-              code={sampleTests ? sampleTests.tests : ""}
+              code={tests.currentTests}
+              language={tests.language}
+              onChange={e => handleTestsEditorChange(e)}
               options={{
-                readOnly: true,
                 lineNumbers: "off",
-                minimap: {
-                  enabled: false
-                }
+                readOnly: false
               }}
             />
           </div>
-          <div>            
+          <div>
             <FormControl>
               <InputLabel>Language</InputLabel>
               <Select
@@ -72,52 +72,60 @@ function sampleTestsView({
               <FormHelperText>Sets the language for the tests</FormHelperText>
             </FormControl>
           </div>
-          <div className="output-console">
-          <MonacoField
-            code={tests.currentTests}
-            language={tests.language}
-            onChange={e => handleTestsEditorChange(e)}
-            options={{
-              lineNumbers: "off",
-              readOnly: false,
-            }}
-            />
-          </div>
           <div>
-            <Button disabled={challengeSubmitted} onClick={() => handleDialogOpening('reset')}>Reset</Button>
-            <Button disabled={challengeSubmitted} onClick={handleRunTestsClick}>Run Tests</Button>
-            <Button disabled={challengeSubmitted} onClick={() => handleDialogOpening('submit')}>Submit</Button>
+            <Button
+              disabled={challengeSubmitted}
+              onClick={() => handleDialogOpening("reset")}>
+              Reset
+            </Button>
+            <Button disabled={challengeSubmitted} onClick={handleRunTestsClick}>
+              Run Tests
+            </Button>
+            <Button
+              disabled={challengeSubmitted}
+              onClick={() => handleDialogOpening("submit")}>
+              Submit
+            </Button>
           </div>
           <Dialog
             open={resetDialogOpen || submitDialogOpen}
-            onClose={() => handleDialogOpening(resetDialogOpen ? 'reset': 'submit')}
-          >
-            <DialogTitle >{resetDialogOpen ? 'Reset tests': 'Submit Challenge'}</DialogTitle>
+            onClose={() =>
+              handleDialogOpening(resetDialogOpen ? "reset" : "submit")
+            }>
+            <DialogTitle>
+              {resetDialogOpen ? "Reset tests" : "Submit Challenge"}
+            </DialogTitle>
             <DialogContent>
               <DialogContentText id="alert-dialog-description">
-              {resetDialogOpen
-                ? 'The changes to the editor and tests will be lost...'
-                : 'Are you sure you want to submit the challenge?'
-              }
+                {resetDialogOpen
+                  ? "The changes to the editor and tests will be lost..."
+                  : "Are you sure you want to submit the challenge?"}
               </DialogContentText>
             </DialogContent>
             <DialogActions>
-              <Button onClick={(resetDialogOpen ? handleReset: handleSubmit)} color="primary">
-                {resetDialogOpen ? 'Reset': 'Submit'}
+              <Button
+                onClick={resetDialogOpen ? handleReset : handleSubmit}
+                color="primary">
+                {resetDialogOpen ? "Reset" : "Submit"}
               </Button>
-              <Button onClick={() => handleDialogOpening(resetDialogOpen ? 'reset': 'submit')} color="primary" autoFocus>
+              <Button
+                onClick={() =>
+                  handleDialogOpening(resetDialogOpen ? "reset" : "submit")
+                }
+                color="primary"
+                autoFocus>
                 Cancel
               </Button>
             </DialogActions>
           </Dialog>
-        </Paper>
+        </div>
       )}
-    
+    </div>
   );
 }
 
 sampleTestsView.propTypes = {
-  handleSelectChange: PropTypes.func.isRequired,
+  handleSelectChange: PropTypes.func.isRequired
 };
 
 export default withStyles(styles)(sampleTestsView);
