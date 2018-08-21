3760c8264f0a783ea21a0ce91e6ac1a81c0446b6
Héctor Daniel Torres <hdt94@hotmail.com>
2018-08-21 13:14:07 -0500

    Output reducer now supports played events with  PLAYBACK_EVENTS_SET

--- src/reducers/output.js
+++ src/reducers/output.js
@@ -1,9 +1,10 @@
 import {
+  PLAYBACK_EVENTS_SET,
   RUN_SAMPLE_TESTS_SUCCESS,
   RUN_SAMPLE_TESTS_ERROR,
   SUBMIT_CHALLENGE_SUCCESS,
   SUBMIT_CHALLENGE_ERROR,
-} from '../actions/types'; 
+} from '../actions/types';
 
 const initialState = {
   testsResults: null,
@@ -11,6 +12,21 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case PLAYBACK_EVENTS_SET: {
+      const { output } = action.payload.eventsData;
+
+      if (!output || output.length === 0) {
+        return {
+          ...state,
+          testsResults: null,
+        };
+      }
+
+      return {
+        ...state,
+        testsResults: output[output.length - 1],
+      };
+    }
     case RUN_SAMPLE_TESTS_SUCCESS:
       return {
         ...state,
